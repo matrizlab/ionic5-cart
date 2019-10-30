653bf4ba9f5da5e9d46d90b95ee14ce98caffbf5
matrizlab <info@matrizlab.com>
2019-10-30 11:30:05 +0100

    cart page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,8 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
-  }
+  },
+  { path: 'cart', loadChildren: './pages/cart/cart.module#CartPageModule' }
 ];
 
 @NgModule({
